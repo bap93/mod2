669736e14d4ab6ebf00ce8f2488744b20c3727ad
bap93 <brookeangelperez@yahoo.com>
2025-01-23 19:17:46 -0500

    Added first and last name variables

--- PerezB_Module2Lab.py
+++ PerezB_Module2Lab.py
@@ -11,33 +11,41 @@
 # * UNQUALIFIED_MESSAGE - a constant storing the message if a student does not qualify for either the honor roll or deans list
 # * INPUT_NAME_MESSAGE - a constant storing the  message asking for the student name input
 # * INPUT_GPA_MESSAGE - a constant storing the message asking for the gpa input 
-# * student - a variable storing the students name 
+# * studentLastName - a variable storing the students last name 
+# * studentFirstName - a variable storing the students first name
 # * gpa - a variable storing the students gpa
 
-student = None 
+studentLastName = None 
+studentFirstName = None
 gpa = 0.0
 
 DEANS_LIST = 3.5
 HONOR_ROLL = 3.25
-DEAN_MESSAGE = "{student} qualifies for the Dean's List."
-HONOR_MESSAGE = "{student} qualifies for the Honor Roll"
-UNQUALIFIED_MESSAGE = "{student} does not qualify for the Dean's List or the Honor Roll"
-INPUT_NAME_MESSAGE = "Please enter the student's name or zzz to quit: "
+DEAN_MESSAGE = "{studentFirstName} {studentLastName} qualifies for the Dean's List."
+HONOR_MESSAGE = "{studentFirstName} {studentLastName} qualifies for the Honor Roll"
+UNQUALIFIED_MESSAGE = "{studentFirstName} {studentLastName} does not qualify for the Dean's List or the Honor Roll"
+INPUT_LAST_MESSAGE = "Please enter the student's last name or zzz to quit: "
+INPUT_FIRST_MESSAGE = "Please enter the student's first name: "
 INPUT_GPA_MESSAGE = "Please enter the student's GPA: "
 
 # loop and get name and gpa and evaulate
 while True: 
     
     # get student name input
-    student = input(INPUT_NAME_MESSAGE) 
+    studentLastName = input(INPUT_LAST_MESSAGE) 
     # check to see if name for student entered
-    if not student: 
-        print("You must enter name for the student, try again.")
+    if not studentLastName: 
+        print("You must enter a name for the student, try again.")
         continue
 
     # check to see if user wants to quit
-    if student.lower() == "zzz": 
+    if studentLastName.lower() == "zzz": 
         break 
+
+    studentFirstName = input(INPUT_FIRST_MESSAGE)
+    if not studentFirstName:
+        print("You must enter a name for the student")
+        continue
     
     # get the gpa input, cast to float, retrieve input again if exception is thrown while trying to cast to float
     gpa = input(INPUT_GPA_MESSAGE) 
@@ -49,8 +57,8 @@ while True:
     
     # check out if the student made the deans list or honor roll
     if gpa >= DEANS_LIST: 
-        print(DEAN_MESSAGE.format(student = student))
+        print(DEAN_MESSAGE.format(studentFirstName = studentFirstName, studentLastName = studentLastName))
     elif gpa >= HONOR_ROLL: 
-        print(HONOR_MESSAGE.format(student = student))
+        print(HONOR_MESSAGE.format(studentFirstName = studentFirstName, studentLastName = studentLastName))
     else: 
-        print(UNQUALIFIED_MESSAGE.format(student = student))
+        print(UNQUALIFIED_MESSAGE.format(studentFirstName = studentFirstName, studentLastName = studentLastName))
